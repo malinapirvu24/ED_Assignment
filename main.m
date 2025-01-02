022cd0202c33337d273392bfd413bbc7a3bd6957
ADAALMAN <Adaalman@outlook.com>
2025-01-02 21:58:21 +0100

    plots

--- main.m
+++ main.m
@@ -3,3 +3,18 @@ close all;
 clc;
 
 load("ClockSync.mat");
+
+estimator = tau + tx_timestamps - rx_timestamps;
+for i = 1:1:6 % different noise variances
+    for j = 1:1:10000 % realisations
+        MSE(j,i) = (-mean(estimator(:, j, i)) - phi_2)^2;
+    end
+end
+figure;
+plot(noise_var, noise_var/10, LineWidth=2)
+hold on
+plot(noise_var, mean(MSE, 1), LineWidth=2)
+grid on
+xlabel("Noise variance")
+ylabel("Error")
+legend("CRLB", "MSE")
